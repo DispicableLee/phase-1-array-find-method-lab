4cc25bf2fef7f62c6d5066f07de52df53b50cd88
DispicableLee <lee.robert053@gmail.com>
2022-09-30 11:08:43 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2014", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+  function superBowlWin(record){    
+    for(const u of record){
+      const eachU = Object.values(u)
+      console.log(eachU)
+      let found = eachU.find(res=>eachU[1]==="W")
+      if(found = eachU.find(res=>eachU[1]==="W")){
+        return found
+      }
+      
+    }
+  }
+
+
+console.log(superBowlWin(record))
